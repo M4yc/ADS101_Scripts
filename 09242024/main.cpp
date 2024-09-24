7374f8587b9ce54ccb3e1268cabb9edf7896aa45
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-09-24 18:22:55 -0300

    :sparkles: Create aula 24-09

---
+++ 09242024/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    cout << "============== Menu ==============" << endl;
+    cout << "        [1] Cadastro Aluno        " << endl;
+    cout << "        [2] Cadastro Professor    " << endl;
+    cout << "        [3] Cadastro Tecnico      " << endl;
+    cout << "        [0] Sair                  " << endl;
+    cout << "==================================" << endl;
+    cout << "Escolha uma opcao: " << endl;
+
+    return 0;
+}
