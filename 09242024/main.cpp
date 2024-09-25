d4a3aa286b1292c1ba32c9e5488372824a00de18
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-09-24 23:42:52 -0300

    :up: Update Aula 24-09

--- 09242024/main.cpp
+++ 09242024/main.cpp
@@ -1,19 +1,95 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int main(){
+class Pessoa{
 
-    cout << "============== Menu ==============" << endl;
-    cout << "        [1] Cadastro Aluno        " << endl;
-    cout << "        [2] Cadastro Professor    " << endl;
-    cout << "        [3] Cadastro Tecnico      " << endl;
-    cout << "        [0] Sair                  " << endl;
-    cout << "==================================" << endl;
-    cout << "Escolha uma opcao: " << endl;
-
+    public:
+        int Id;
+        string Nome;
+        int CPF;
     
+    void CadastrarPessoa(){
 
+    }
+    void ExibirPessoa(){
+        cout << "\n_____________________________" << endl;
+        cout << "        Nome: "<< Nome <<endl;
+        cout << "        Id: "<< Id <<endl;
+        cout << "        CPF: "<< CPF <<endl;
+
+    }
+    
+};
+
+class Aluno: public Pessoa{
+    public:
+        int matricula;
+        string Curso;
+        float Mensalidade;
+        float notas[3];
+    
+    float CalcMedia(){
+        float soma = 0;
+        for( float nota : notas)
+        {
+            soma += nota;
+        }
+        
+        return soma / 3;
+    }
+
+    string VereAprov(){
+        if (CalcMedia() >= 60.0)
+        {
+            return "Aprovado";
+        }else{
+            return "Reprovado";
+        }
+    }
+
+    void ExibirInfo(){
+        Pessoa :: ExibirPessoa();
+        cout << "     Matricula: "<< matricula <<endl;
+        cout << "     Media: "<< CalcMedia() <<endl;
+        cout << "     Status: "<< VereAprov() <<endl;
+    }
+
+};
+
+
+int main(){
+    Pessoa pessoa;
+
+    int opcao;
+    do{
+        cout << "============== Menu ==============" << endl;
+        cout << "        [1] Cadastro Aluno        " << endl;
+        cout << "        [2] Cadastro Professor    " << endl;
+        cout << "        [3] Cadastro Tecnico      " << endl;
+        cout << "        [0] Sair                  " << endl;
+        cout << "==================================" << endl;
+        cout << "Escolha uma opcao: ";
+        cin >> opcao;
+        switch (opcao){
+        case 1:
+            pessoa.ExibirPessoa();
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 0:
+            cout << "Saindo do sistema...." << endl;
+            break;
+        default:
+            cout << "Opcao envalida! Tente novamente" << endl;
+        }
+    } while (opcao != 0);
+    
     return 0;
 }
